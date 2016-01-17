f674d510043494933da7c544a4aaf64cfe7e8a6a
Magnus Rex <magnus@pugglepay.com>
2016-01-17 21:42:46 +0100

    Fix tests.

--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,8 @@ config :logger, level: :warn
 # Configure your database
 config :elm_base, ElmBase.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "rexikan",
+  password: "",
   database: "elm_base_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule ElmBase.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "<div id=\"elm-container\"></div>"
   end
 end
