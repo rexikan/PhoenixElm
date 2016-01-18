eb10effa044dbc9dec7026650b458e097f2ea0d2
Magnus Rex <magnus@pugglepay.com>
2016-01-18 23:03:22 +0100

    Change elm code to StartApp.Simple example.

--- web/elm/App.elm
+++ web/elm/App.elm
@@ -1,7 +1,40 @@
 module App where
 
-import Html exposing (text)
+import Html exposing (..)
+import Html.Events exposing (onClick)
+import StartApp.Simple as StartApp
 
-main: Html.Html
+
+main : Signal Html
 main =
-  text "Hello from Elm!"
+  StartApp.start { model = model, view = view, update = update }
+
+
+-- MODEL
+
+type alias Model = Int
+
+model: Model
+model = 0
+
+
+-- UPDATE
+
+type Action = Increment | Decrement
+
+update: Action -> Model -> Model
+update action model =
+  case action of
+    Increment -> model + 1
+    Decrement -> model - 1
+
+
+-- VIEW
+
+view : Signal.Address Action -> Model -> Html
+view address model =
+  div []
+    [ button [ onClick address Decrement ] [ text "-" ]
+    , div [] [ text (toString model) ]
+    , button [ onClick address Increment ] [ text "+" ]
+    ]
